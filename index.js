6f38287cf537568f8df85116a72424c3f04de6bd
Greg Morgan <gregm9999@gmail.com>
2019-01-12 04:31:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ function Driver(name, age, experience) {
   this.experience=experience;
 }
 
-function PickupLocation(PickupLocation, address, city) {
-  this.PickupLocation=PickupLocation;
+function PickupLocation(address, city) {
   this.address=address;
   this.city=city;
 }
